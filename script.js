c5a008a18052237d9952942cf4165f16b0f20511
Alejandro9670 <onlinesystemchile@gmail.com>
2025-11-27 02:25:09 -0300

    Updated index.html

--- script.js
+++ script.js
@@ -376,11 +376,6 @@ if (videoIframe) {
 // Video Modal Functions
 function openVideoModal() {
     const modal = document.getElementById('videoModal');
-    const iframe = document.getElementById('videoIframe');
-
-    // Set the YouTube embed URL with autoplay
-    iframe.src = 'https://www.youtube.com/embed/BdWjoRQitwY?si=E8oY1zFeDjdiR3AV&autoplay=1';
-
     modal.classList.add('active');
     document.body.style.overflow = 'hidden';
 }
@@ -389,8 +384,10 @@ function closeVideoModal() {
     const modal = document.getElementById('videoModal');
     const iframe = document.getElementById('videoIframe');
 
-    // Stop video by clearing the src
+    // Stop video by reloading the iframe
+    const currentSrc = iframe.src;
     iframe.src = '';
+    iframe.src = currentSrc;
 
     modal.classList.remove('active');
     document.body.style.overflow = 'auto';
